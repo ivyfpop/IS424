5e760210200d7975bba6e7b7b9b745c6a5007e59
LiamMahoney <liammahoney96@gmail.com>
2018-04-22 17:49:11 -0500

    moar

--- myFunctions.php
+++ myFunctions.php
@@ -6,19 +6,25 @@
   //Query for all events member has not signed up for
   //assuming query result var is notSignedUp
 
+  echo"<div class='container header'>Sign Up</div>";
+
   while ($row =  $notSignedUp->fetch_row()) {
     echo "<div class='card'><div class='card-body'>";
     echo $row['function_Description']. " " . $row['function_Location'] . " " . $row['function_Date'];
     echo "</div></div>";
   }
+  // clear results
 
   //Query for all past events
+
+  echo"<div class='container header'>Past Events</div>";
 
   while ($row = $pastEvents->fetch_row()) {
     echo "<div class='card'><div class='card-body'>";
     echo $row['function_Description']. " " . $row['function_Location'] . " " . $row['function_Date'];
     echo "</div></div>";
   }
+  //clear result
 
 
 
