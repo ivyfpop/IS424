2b1b2150218bd8cdc61a5c5ede257b7ad055ff0b
Michael Fink <mfink4@wisc.edu>
2018-04-22 23:07:21 -0500

    Update Account Layout

--- accountPanel.php
+++ accountPanel.php
@@ -4,28 +4,25 @@
     <?php include 'helper/header.php' ?> 
         
 	<body>
-        <form class="form-signin" action='helper/accountHelper.php' name='update' method='post'>
-            <div class="form-label-group">
+        <form class="container" action='helper/accountHelper.php' name='update' method='post'>
+        
+            <div class="form-row">
                 <input type="text" id="inputFirstName" class="form-control" name='first_Name' placeholder="First Name" required autofocus>
                 <label for="inputFirstName">First Name</label>
-            </div>
-
-            <div class="form-label-group">
+                
                 <input type="text" id="inputLastName" class="form-control" name='last_Name' placeholder="Last Name" required autofocus>
-                <label for="inputLastName">Last Name</label>
+                <label for="inputLastName">Last Name</label>                
             </div>
             
-            <div class="form-label-group">
+            <div class="form-row">
                 <input type="email" id="inputEmail" class="form-control" name='email' placeholder="Email Address" required autofocus>
                 <label for="inputEmail">Email Address</label>
-            </div>
-            
-            <div class="form-label-group">
+                
                 <input type="password" id="inputPassword" class="form-control" name='password' placeholder="Password" required>
                 <label for="inputPassword">Password</label>
             </div>
             
-            <div class='form-label-group form-check form-check-inline'>
+            <div class='form-row form-check form-check-inline'>
                 <input class='form-check-input' type='checkbox' id='isSprinter' value='isSprinter'>
                 <label class='form-check-label' for='isSprinter'>Sprinter</label>
                 
@@ -39,7 +36,7 @@
                 <label class='form-check-label' for='isJumper'>Jumper</label>
             </div>
             
-            <button class="btn btn-lg btn-success btn-block" type="submit" name='update' value='update'>Update Account</button>
+            <button class="form-row btn btn-lg btn-success btn-block" type="submit" name='update' value='update'>Update Account</button>
         </form>
     </body>
 </html>
