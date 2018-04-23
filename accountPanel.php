c770e1b862a16240f2cc44f60552f0ec64175dd3
Michael Fink <mfink4@wisc.edu>
2018-04-22 23:27:57 -0500

    I'll fix it later.

--- accountPanel.php
+++ accountPanel.php
@@ -6,31 +6,27 @@
 	<body>
         <form class="container form-signin" action='helper/accountHelper.php' name='update' method='post'>
         
-            <div class="form-row">
-                <div class="form-label-group">
-                    <input type="text" id="inputFirstName" class="form-control" name='first_Name' placeholder="First Name" required autofocus>
-                    <label for="inputFirstName">First Name</label>
-                </div>
-                
-                <div class="form-label-group">
-                    <input type="text" id="inputLastName" class="form-control" name='last_Name' placeholder="Last Name" required autofocus>
-                    <label for="inputLastName">Last Name</label>                
-                </div>
+            <div class="form-label-group">
+                <input type="text" id="inputFirstName" class="form-control" name='first_Name' placeholder="First Name" required autofocus>
+                <label for="inputFirstName">First Name</label>
             </div>
             
-            <div class="form-row">
-                <div class="form-label-group">
-                    <input type="email" id="inputEmail" class="form-control" name='email' placeholder="Email Address" required autofocus>
-                    <label for="inputEmail">Email Address</label>
-                </div>
-                
-                <div class="form-label-group">                
-                    <input type="password" id="inputPassword" class="form-control" name='password' placeholder="Password" required>
-                    <label for="inputPassword">Password</label>
-                </div>
+            <div class="form-label-group">
+                <input type="text" id="inputLastName" class="form-control" name='last_Name' placeholder="Last Name" required autofocus>
+                <label for="inputLastName">Last Name</label>                
+            </div>
+        
+            <div class="form-label-group">
+                <input type="email" id="inputEmail" class="form-control" name='email' placeholder="Email Address" required autofocus>
+                <label for="inputEmail">Email Address</label>
             </div>
             
-            <div class='form-row form-check form-check-inline'>
+            <div class="form-label-group">                
+                <input type="password" id="inputPassword" class="form-control" name='password' placeholder="Password" required>
+                <label for="inputPassword">Password</label>
+            </div>
+            
+            <div class='form-check form-check-inline'>
                 <input class='form-check-input' type='checkbox' id='isSprinter' value='isSprinter'>
                 <label class='form-check-label' for='isSprinter'>Sprinter</label>
                 
@@ -44,9 +40,8 @@
                 <label class='form-check-label' for='isJumper'>Jumper</label>
             </div>
             
-            <div class="form-row">
-                <button class="btn btn-lg btn-success btn-block" type="submit" name='update' value='update'>Update Account</button>
-            </div>
+            <button class="btn btn-lg btn-success btn-block" type="submit" name='update' value='update'>Update Account</button>
+            
         </form>
     </body>
 </html>
