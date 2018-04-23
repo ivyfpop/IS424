2d3fcc0b619df67b5d4f0a192ec91f34b954d387
Michael Fink <mfink4@wisc.edu>
2018-04-22 22:48:30 -0500

    Fixed the heade rfinally..
    The login.css file modifies the dimensions of the website.

--- helper/header.php
+++ helper/header.php
@@ -17,7 +17,7 @@
     <link href="helper/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
-
+    <link href="helper/css/styles.css" rel="stylesheet">
     <!-- End Bootstrap / Core CSS -->
 
     <!-- navigation bar -->
