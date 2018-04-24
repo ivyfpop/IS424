d682ba72f759cbfe767e2b8e64810bd669fadd0b
Michael Fink <mfink4@wisc.edu>
2018-04-23 21:17:22 -0500

    Formatted Date?

--- myTransactions.php
+++ myTransactions.php
@@ -15,26 +15,28 @@
         // Open Transactions Header
         echo"<h2 class='text-center'><strong>Open Transactions</strong></h2>";
 
-          // Print out each of the transactions
-          while ($row = mysqli_fetch_array($openTransactions, MYSQLI_BOTH)){
- 
-            echo"
-            <div class='card border-warning mb-3'>
-                <div class='card-header'>
-                  <h5 class='mb-0'>
-                    <button class='btn btn-link' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
-                        <strong>Transaction Date:</strong> $row[transactionInitDate] <strong> Amount:</strong> $$row[transactionQuantity]
-                    </button>
-                  </h5>
-                </div>
+            // Print out each of the transactions
+            while ($row = mysqli_fetch_array($openTransactions, MYSQLI_BOTH)){
+
+                $transactionInitDate = date("m/d/y g:i A", strtotime($row[transactionInitDate]));
                 
-                <div id='$row[transactionID]' class='collapse'>
-                  <div class='card-body'>
-                    <strong>Description:</strong>
-                    $row[transactionDescription]
-                  </div>
-                </div>
-              </div>";             
+                echo"
+                <div class='card border-warning mb-3'>
+                    <div class='card-header'>
+                      <h5 class='mb-0'>
+                        <button class='btn btn-link' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
+                            <strong>Transaction Date:</strong> $transactionInitDate <strong> Amount:</strong> $$row[transactionQuantity]
+                        </button>
+                      </h5>
+                    </div>
+                    
+                    <div id='$row[transactionID]' class='collapse'>
+                      <div class='card-body'>
+                        <strong>Description:</strong>
+                        $row[transactionDescription]
+                      </div>
+                    </div>
+                  </div>";             
           }
       }
 ?>
