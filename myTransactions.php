13cfa33867ac96e567f51d36ed57ac9c8115a664
Michael Fink <mfink4@wisc.edu>
2018-04-23 23:02:14 -0500

    Filled out Transaction Card

--- myTransactions.php
+++ myTransactions.php
@@ -12,8 +12,8 @@
         // Verify there are open transactions for this member
         if($openTransactions = $db->query("SELECT * FROM Transaction WHERE transactionPaymentDate IS NULL AND memberID = '$_SESSION[memberID]'")){
 
-        // Open Transactions Header
-        echo"<h2 class='text-center'><strong>Open Transactions</strong></h2>";
+            // Open Transactions Header
+            echo"<h2 class='text-center'><strong>Open Transactions</strong></h2>";
 
             // Print out each of the transactions
             while ($row = mysqli_fetch_array($openTransactions, MYSQLI_BOTH)){
@@ -23,20 +23,36 @@
                 echo"
                 <div class='card border-warning mb-3'>
                     <div class='card-header'>
-                    
-                            <button class='btn btn-link float-left h2' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
-                                <strong>$transactionInitDate</strong> $$row[transactionQuantity]
-                            </button> 
-                           
+                            <button class='btn btn-link float-left' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
+                                <h3>$transactionInitDate - $$row[transactionQuantity]</h3>
+                            </button>
                             <form action='venmo.php' name='transaction' method='post'>
-                                <button class='btn btn-success float-right' type='submit' name='transaction' value='$row[transactionID]'>Pay Now!</button>
+                                <button class='btn btn-success float-right' type='submit' name='transaction' value='$row[transactionID]'>
+                                    <h3>Pay Now!</h3>
+                                </button>
                             </form>
                     </div>
                     
                     <div id='$row[transactionID]' class='collapse'>
                       <div class='card-body'>
-                        <strong>Description:</strong>
-                        $row[transactionDescription]
+                        <strong>Transaction ID:</strong> $row[transactionID]
+                        </br>
+                        <strong>Request Date:</strong> $row[transactionInitDate]
+                        </br>";
+                        // If there is an Event associated with the transaction
+                        if($row[eventID]){
+                            //Determine the name of the event
+                            $eventRow = mysqli_fetch_array("SELECT eventName from Event", MYSQLI_BOTH);
+                            echo"<strong>Event:</strong> $eventRow[eventName]
+                                 </br>";
+                        }
+
+                        
+                    echo"<strong>Amount:</strong> $row[transactionQuantity]
+                        </br>                        
+                        <strong>Category:</strong> $row[transactionCategory]
+                        </br>                        
+                        <strong>Description:</strong>$row[transactionDescription]
                       </div>
                     </div>
                   </div>";             
