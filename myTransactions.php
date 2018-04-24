fd487428e14918dd3feecfeb59b71c11ac70008b
Michael Fink <mfink4@wisc.edu>
2018-04-24 00:56:37 -0500

    Working Transactions Page?

--- myTransactions.php
+++ myTransactions.php
@@ -5,7 +5,7 @@
     <div class='container bg-faded p-4 my-4'>
     <h1 class='text-center'><strong>My Transactions</strong></h1>
 
-<?php 
+    <?php 
         include 'helper/connect.php';
         session_start();
 
@@ -60,8 +60,59 @@
                         </div>
                     </div>
                 </div>";             
-          }
-      }
+            }
+        }
+        // Verify there are Pending transactions for this member ADD ORDER BY payment date to keep them in order.
+        if($pastTransactions = $db->query("SELECT * FROM Transaction WHERE transactionPaymentDate IS NOT NULL AND transactionApprovalDate IS NULL AND memberID = '$_SESSION[memberID]'")){
+
+            // Open Transactions Header
+            echo"<h2 class='text-center'><strong>Pending Transactions</strong></h2>";
+
+            // Print out each of the past transactions
+            while ($row = mysqli_fetch_array($pastTransactions, MYSQLI_BOTH)){
+
+                $transactionInitDate = date("m/d/y g:i A", strtotime($row[transactionInitDate]));
+                $transactionPaymentDate = date("m/d/y g:i A", strtotime($row[transactionPaymentDate]));
+                
+                echo"
+                <div class='card mb-3 border-warning'>
+                    <div class='card-header bg-warning'>
+                            <button class='btn btn-link text-white float-left' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
+                                <h3>PENDING - $$row[transactionQuantity]</h3>
+                            </button>
+                    </div>
+                    
+                    <div id='$row[transactionID]' class='collapse'>
+                      <div class='card-body border-warning'>
+                        <strong>Transaction ID:</strong> $row[transactionID]
+                        </br>
+                        <strong>Request Date:</strong> $transactionInitDate
+                        </br>
+                         <strong>Payment Date:</strong> $transactionPaymentDate
+                        </br>";
+                        
+                        // If there is an Event associated with the transaction
+                        if ($row[eventID]){
+                            //Determine the name of the event
+                            $eventResult = $db->query("SELECT eventName FROM Event WHERE eventID = $row[eventID]");
+                            $eventRow = mysqli_fetch_array($eventResult, MYSQLI_BOTH);
+                            echo"<strong>Event Name:</strong> $eventRow[eventName]
+                                 </br>";
+                        }
+                        // Otherwise print out the category for the event
+                        else{
+                            echo"<strong>Category:</strong> $row[transactionCategory]
+                                 </br>";
+                        }
+                        
+                       echo"<strong>Amount:</strong> $$row[transactionQuantity]
+                            </br>                                            
+                            <strong>Description:</strong> $row[transactionDescription]
+                        </div>
+                    </div>
+                </div>";             
+            }
+        }
 
         // Verify there are Past transactions for this member ADD ORDER BY payment date to keep them in order.
         if($pastTransactions = $db->query("SELECT * FROM Transaction WHERE transactionPaymentDate IS NOT NULL AND transactionApprovalDate IS NOT NULL AND memberID = '$_SESSION[memberID]'")){
@@ -74,6 +125,7 @@
 
                 $transactionInitDate = date("m/d/y g:i A", strtotime($row[transactionInitDate]));
                 $transactionPaymentDate = date("m/d/y g:i A", strtotime($row[transactionPaymentDate]));
+                $transactionApprovalDate = date("m/d/y g:i A", strtotime($row[transactionApprovalDate]));
                 
                 echo"
                 <div class='card mb-3 border-success'>
@@ -120,11 +172,9 @@
                         </div>
                     </div>
                 </div>";             
-          }
-      }
-
-      
-?>
+            }
+        }  
+    ?>
     </div>
 
     <!-- JS Used -->
