3cedf0496084ccb70ec8183628b3ba9bd718ffab
Michael Fink <mfink4@wisc.edu>
2018-04-23 22:18:50 -0500

    Formatting sucks

--- myTransactions.php
+++ myTransactions.php
@@ -22,13 +22,13 @@
                 
                 echo"
                 <div class='card border-warning mb-3'>
-                    <div class='card-header row'>
-                            <button class='btn btn-link' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
+                    <div class='card-header'>
+                            <button class='btn btn-link mx-auto' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
                                 <strong>$transactionInitDate</strong> $$row[transactionQuantity]
                             </button> 
                            
                             <form action='venmo.php' name='transaction' method='post'>
-                                <button class='btn btn-success ml-auto' type='submit' name='transaction' value='$row[transactionID]'>Pay Now!</button>
+                                <button class='btn btn-success mx-auto' type='submit' name='transaction' value='$row[transactionID]'>Pay Now!</button>
                             </form>
                     </div>
                     
