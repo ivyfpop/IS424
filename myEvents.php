ca3d984a49d73380dc6d6cc09bbcdd40e2d1e5d8
LiamMahoney <liammahoney96@gmail.com>
2018-04-23 19:19:59 -0500

    more

--- myEvents.php
+++ myEvents.php
@@ -5,15 +5,15 @@ include 'helper/header.php';
 include 'helper/connect.php';
 
 //$registeredID is needed here - might need to query for
-$regMemQuery = "SELECT registeredID FROM Registered_Member WHERE memberID = {$SESSION['memberID']} ORDER BY registeredSeason + 1 DESC";
-echo "TESTING: regMemQuery: {$regMemQuery}";
+$regMemQuery = "SELECT registeredID FROM Registered_Member WHERE memberID = '$SESSION['memberID']' ORDER BY registeredSeason + 1 DESC";
+echo "TESTING: regMemQuery: '$regMemQuery'";
 $registeredIDResult = mysqli_query($db, $regMemQuery);
 $registeredID = null;
-echo "TESTING: registeredID num rows = {$registeredIDResult->num_rows}";
+echo "TESTING: registeredID num rows: '$registeredIDResult->num_rows'";
 if ($registeredIDResult->num_rows != 0) {
   $registeredIDRow = mysqli_fetch_array($registeredIDResult);
   $registeredID = $registeredIDRow[0];
-  echo "TESTING: registered ID : {$registeredID}";
+  echo "TESTING: registered ID: '$registeredID'";
 }
 
 if ($registeredID !== null) {
@@ -23,7 +23,9 @@ if ($registeredID !== null) {
       - $signedUpArr = all eventIDs from query
 
    */
-   $signedUpResults = mysqli_query($db, "SELECT * FROM Registered_Member_Event WHERE registeredID = {$registeredID}");
+   
+   $signedUpResults = mysqli_query($db, "SELECT * FROM Registered_Member_Event WHERE registeredID = '$registeredID'");
+
    //Query results and display info for each event that is signed up for and add eventID to $signedUpArr
 
 
