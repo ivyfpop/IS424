3802306d04c19da82eaaa57638f256861236ba6f
LiamMahoney <liammahoney96@gmail.com>
2018-04-30 13:49:27 -0500

    General update idea

--- eventSignUp.php
+++ eventSignUp.php
@@ -1,8 +1,19 @@
 <?php
-echo $_POST['registeredID'];
-//TODO registeredID and eventID are in $_POST. Use to query for event details and
-//insert into the database.
+/*
+  registeredID and eventID in $_POST.
 
-//TODO make form that has all attributes of Registered_Member_Event and submit it to DB
+  TODO: Display all information about the event the user is signing up for
+  TODO: If member has a date in driverAuthorizationDate, ask for seats left in car
+  TODO: Create instance in Registered_Member_Event with proper registeredID, eventID
+          -Figure out how transactionID will be populated - some events don't have
+           transactions
+          -Should prices be set in Event table so we can tell the user how expensive
+           the event is?
+  TODO: Redirect to myEvents page when submitted (catch $_POST on myEvents.php page -
+        this is where the update will actually happen)
+
+*/
+
+
 
  ?>
--- myEvents.php
+++ myEvents.php
@@ -8,86 +8,127 @@
   <div class='container bg-faded p-4 my-4'>
   <h1 class='text-center'><strong>My Events</strong></h1>
 
-    <?php
+<?php
 
 
-      //Querying for registeredID from Registered_Member with memberID
-      $regMemQuery = "SELECT registeredID FROM Registered_Member WHERE memberID = '$_SESSION[memberID]' ORDER BY registeredSeason DESC";
-      $registeredIDResult = mysqli_query($db, $regMemQuery);
-      $registeredID = null;
-      if ($registeredIDResult->num_rows != 0) {
-        $registeredIDRow = mysqli_fetch_array($registeredIDResult);
-        $registeredID = $registeredIDRow[0];
-      }
+  //Querying for registeredID from Registered_Member with memberID
+  $regMemQuery = "SELECT registeredID FROM Registered_Member WHERE memberID = '$_SESSION[memberID]' ORDER BY registeredSeason DESC";
+  $registeredIDResult = mysqli_query($db, $regMemQuery);
+  $registeredID = null;
+  if ($registeredIDResult->num_rows != 0) {
+    $registeredIDRow = mysqli_fetch_array($registeredIDResult);
+    $registeredID = $registeredIDRow[0];
+  }
 
-       // TODO: If registeredID doesn't exist need to decide which info to display
-       // for sure can't show signed up for - could show events to sign up for
-      if ($registeredID !== null) {
+   // TODO: If registeredID doesn't exist need to decide which info to display
+   // for sure can't show signed up for - could show events to sign up for
+  if ($registeredID !== null) {
 
-        /*
-         Events to Sign up for
-           - check to make sure eventID isn't in $signedUpArr before displaying
-        */
-        $signUpForResults = mysqli_query($db, "SELECT * FROM Event WHERE eventID NOT IN (
-        SELECT eventID from Registered_Member_Event WHERE registeredID = '$registeredID')");
+    /*
+     Events to Sign up for
+       - check to make sure eventID isn't in $signedUpArr before displaying
+    */
+    $signUpForResults = mysqli_query($db, "SELECT * FROM Event WHERE eventID NOT IN (
+    SELECT eventID from Registered_Member_Event WHERE registeredID = '$registeredID')");
 
-        if ($signUpForResults != 'FALSE') {
-          echo"<hr><h2 class='text-center'><strong>Sign Up For Events</strong></h2><hr>";
-          while($row = mysqli_fetch_array($signUpForResults, MYSQLI_BOTH)) {
+    if ($signUpForResults != 'FALSE') {
+      echo"<hr><h2 class='text-center'><strong>Sign Up For Events</strong></h2><hr>";
+      while($row = mysqli_fetch_array($signUpForResults, MYSQLI_BOTH)) {
 //TODO add sign up button -> handle sign up :)
-            echo"
-            <div class='card mb-3 border-success'>
-              <div class='card-header bg-success'>
-                <button class='btn btn-link text-white float-left' type='button' data-toggle='collapse' data-target='#$row[eventID]'>
-                    <h3>$row[eventName]</h3>
-                </button>
-                </div>
-                <div id='$row[eventID]' class='collapse'>
-                  <div class='card-body border-success'>
-                    <strong>Event ID:</strong> $row[eventID]
-                    </br>
-                    <strong>Event Category:</strong> $row[eventCategory]
-                    </br>
-                    <strong>Date:</strong> $row[eventDate]
-                    </br>
-                    <strong>Location:</strong> $row[eventAddress], $row[eventCity], $row[eventState], $row[eventZip]
-                    </br>
-                    <strong>Description:</strong> $row[eventBio]
-                    </br>
-                    <form action='eventSignUp.php' name='eventSignUp' method='post'>
-                      <input type='hidden' name='registeredID' id='registeredID' value=$registeredID>
-                      <input type='hidden' name='eventID' value=$row[eventID]>
-                      <button type='submit' class='btn btn-danger'>Sign Up</button>
-                    </form>
-                    </br>
-                  </div>
-                </div>
-              </div>";
-              //TODO sign up button
+        echo"
+        <div class='card mb-3 border-success'>
+          <div class='card-header bg-success'>
+            <button class='btn btn-link text-white float-left' type='button' data-toggle='collapse' data-target='#$row[eventID]'>
+                <h3>$row[eventName]</h3>
+            </button>
+            </div>
+            <div id='$row[eventID]' class='collapse'>
+              <div class='card-body border-success'>
+                <strong>Event ID:</strong> $row[eventID]
+                </br>
+                <strong>Event Category:</strong> $row[eventCategory]
+                </br>
+                <strong>Date:</strong> $row[eventDate]
+                </br>
+                <strong>Location:</strong> $row[eventAddress], $row[eventCity], $row[eventState], $row[eventZip]
+                </br>
+                <strong>Description:</strong> $row[eventBio]
+                </br>
+                <form action='eventSignUp.php' name='eventSignUp' method='post'>
+                  <input type='hidden' name='registeredID' id='registeredID' value=$registeredID>
+                  <input type='hidden' name='eventID' value=$row[eventID]>
+                  <button type='submit' class='btn btn-danger'>Sign Up</button>
+                </form>
+                </br>
+              </div>
+            </div>
+          </div>";
+          //TODO sign up button
 
+      }
+    } else {
+      echo"<div class='container'>There are no events for you to sign up for at this time. Please check back soon!</div>";
+    }
+
+    //TODO add isComplete = 0 to query. make another query that has isComplete = 1
+
+    //Querying for all events in Registered_Member_Event with user's registeredID
+    $signedUpResults = mysqli_query($db, "SELECT * FROM Registered_Member_Event
+      INNER JOIN Event ON Registered_Member_Event.eventID=Event.eventID WHERE registeredID =
+      '$registeredID' AND isComplete = 0");
+      /*
+        Current Events display.
+        A current event is one that is in the Registered_Member_Event table and
+        has isComplete set to 0. This means that the event has not happened yet.
+      */
+    if ($signedUpResults != 'FALSE') { //Making sure that query returns data
+      echo"<hr><h2 class='text-center'><strong>Events Signed Up For</strong></h2><hr>";
+      while($row = mysqli_fetch_array($signedUpResults, MYSQLI_BOTH)){
+        //Events that have not been completed (they haven't happened yet)
+          echo"
+          <div class='card mb-3 border-success'>
+            <div class='card-header bg-success'>
+              <button class='btn btn-link text-white float-left' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
+                  <h3>$row[eventName]</h3>
+              </button>
+              </div>
+              <div id='$row[transactionID]' class='collapse'>
+                <div class='card-body border-success'>
+                  <strong>Transaction ID:</strong> $row[transactionID]
+                  </br>
+                  <strong>Event Category:</strong> $row[eventCategory]
+                  </br>
+                  <strong>Date:</strong> $row[eventDate]
+                  </br>
+                  <strong>Location:</strong> $row[eventAddress], $row[eventCity], $row[eventState], $row[eventZip]
+                  </br>
+                  <strong>Leave By:</strong> $row[leaveBy]
+                  </br>
+                  <strong>Description:</strong> $row[eventBio]
+                  </br>
+                </div>
+              </div>
+            </div>";
           }
         } else {
-          echo"<div class='container'>There are no events for you to sign up for at this time. Please check back soon!</div>";
+          echo"<div class='container' style='text-align:center;'>You are not signed up for any events</div>";
         }
-
-        //TODO add isComplete = 0 to query. make another query that has isComplete = 1
-
-        //Querying for all events in Registered_Member_Event with user's registeredID
-        $signedUpResults = mysqli_query($db, "SELECT * FROM Registered_Member_Event
+        //TODO do we want this to show all past events for the memeber ID instead
+        //of just the registeredID?
+        $pastEvents = mysqli_query($db, "SELECT * FROM Registered_Member_Event
           INNER JOIN Event ON Registered_Member_Event.eventID=Event.eventID WHERE registeredID =
-          '$registeredID' AND isComplete = 0");
+          '$registeredID' AND isComplete = 1");
           /*
-            Current Events display.
-            A current event is one that is in the Registered_Member_Event table and
-            has isComplete set to 0. This means that the event has not happened yet.
+           Past Events Display.
+           A past event is one that has already happened, meaning the attribute isComplete is
+           set to 1 in the database.
           */
-        if ($signedUpResults != 'FALSE') { //Making sure that query returns data
-          echo"<hr><h2 class='text-center'><strong>Events Signed Up For</strong></h2><hr>";
-          while($row = mysqli_fetch_array($signedUpResults, MYSQLI_BOTH)){
-            //Events that have not been completed (they haven't happened yet)
+          echo"<hr><h2 class='text-center'><strong>Past Events</strong></h2><hr>";
+          if ($pastEvents != 'FALSE') {
+            while($row = mysqli_fetch_array($pastEvents, MYSQLI_BOTH)){
               echo"
-              <div class='card mb-3 border-danger'>
-                <div class='card-header bg-danger'>
+              <div class='card mb-3 border-success'>
+                <div class='card-header bg-success'>
                   <button class='btn btn-link text-white float-left' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
                       <h3>$row[eventName]</h3>
                   </button>
@@ -109,61 +150,20 @@
                     </div>
                   </div>
                 </div>";
-              }
-            } else {
-              echo"<div class='container' style='text-align:center;'>You are not signed up for any events</div>";
             }
-            //TODO do we want this to show all past events for the memeber ID instead
-            //of just the registeredID?
-            $pastEvents = mysqli_query($db, "SELECT * FROM Registered_Member_Event
-              INNER JOIN Event ON Registered_Member_Event.eventID=Event.eventID WHERE registeredID =
-              '$registeredID' AND isComplete = 1");
-              /*
-               Past Events Display.
-               A past event is one that has already happened, meaning the attribute isComplete is
-               set to 1 in the database.
-              */
-              echo"<hr><h2 class='text-center'><strong>Past Events</strong></h2><hr>";
-              if ($pastEvents != 'FALSE') {
-                while($row = mysqli_fetch_array($pastEvents, MYSQLI_BOTH)){
-                  echo"
-                  <div class='card mb-3 border-success'>
-                    <div class='card-header bg-success'>
-                      <button class='btn btn-link text-white float-left' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
-                          <h3>$row[eventName]</h3>
-                      </button>
-                      </div>
-                      <div id='$row[transactionID]' class='collapse'>
-                        <div class='card-body border-success'>
-                          <strong>Transaction ID:</strong> $row[transactionID]
-                          </br>
-                          <strong>Event Category:</strong> $row[eventCategory]
-                          </br>
-                          <strong>Date:</strong> $row[eventDate]
-                          </br>
-                          <strong>Location:</strong> $row[eventAddress], $row[eventCity], $row[eventState], $row[eventZip]
-                          </br>
-                          <strong>Leave By:</strong> $row[leaveBy]
-                          </br>
-                          <strong>Description:</strong> $row[eventBio]
-                          </br>
-                        </div>
-                      </div>
-                    </div>";
-                }
-              } else {
-                echo"<div class='container' style='text-align:center;'>There are no past results to display for this account. </div>";
-              }
-      } else {
-          // Not Registered Header
-          echo"<h2 class='text-center'><strong>You Are Not Registered!</strong></h2>";
+          } else { //TODO all else statements here are not working like expected.
+            echo"<div class='container' style='text-align:center;'>There are no past results to display for this account. </div>";
+          }
+  } else {
+      // Not Registered Header
+      echo"<h2 class='text-center'><strong>You Are Not Registered!</strong></h2>";
 
-          // ?? display all events with disabled signup buttons
-      }
-      mysqli_close($db);
+      // ?? display all events with disabled signup buttons
+  }
+  mysqli_close($db);
 
 
-    ?>
+?>
   </div>
   <?php include 'helper/footer.php' ?>
 </html>
