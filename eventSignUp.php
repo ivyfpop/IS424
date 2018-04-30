e7ec1d0494cc9873d9ed0a5e2b624a4cabf8845a
LiamMahoney <liammahoney96@gmail.com>
2018-04-30 09:52:17 -0500

    Comments

--- eventSignUp.php
+++ eventSignUp.php
@@ -3,4 +3,6 @@ echo $_POST['registeredID'];
 //TODO registeredID and eventID are in $_POST. Use to query for event details and
 //insert into the database.
 
+//TODO make form that has all attributes of Registered_Member_Event and submit it to DB
+
  ?>
