0976a820e66f609273ee187b7a9a0c7905835260
Michael Fink <mfink4@wisc.edu>
2018-04-29 20:07:08 -0500

    ?

--- transactionManagement.php
+++ transactionManagement.php
@@ -60,7 +60,8 @@
                 $approveQuery = "UPDATE Transaction SET transactionPaymentDate = NOW(), transactionApprovalDate = NOW(), transactionApprovalMemberID = '$memberID' WHERE transactionID = '$transactionID'";
                 $db->query($approveQuery);
                 mysqli_close();
-                $transactionQuery = "$_POST['transactionQuery']";
+                $transactionQuery = $_POST['transactionQuery'];
+                echo"<p>$transactionQuery</p>";
             }
                 
                 
