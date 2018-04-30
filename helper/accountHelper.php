0edacf68a33edb28b616a6eeb5b6f11d130f4f08
jfortman4 <jfortman22@gmail.com>
2018-04-30 08:30:11 -0500

    update functionality

--- helper/accountHelper.php
+++ helper/accountHelper.php
@@ -55,10 +55,7 @@
 
             include 'connect.php';
             // Query used to update the account
-            $updateQuery = "UPDATE Member SET (firstName, lastName, email, password, isSprinter, isDistance, isThrower, isJumper)
-                            VALUES ('$_POST[firstName]','$_POST[lastName]','$_POST[email]','$_POST[password]',
-                            '$_POST[isSprinter]','$_POST[isDistance]','$_POST[isThrower]','$_POST[isDistance]')
-                            WHERE memberID = '$_SESSION['memberID']'";
+            $updateQuery = "UPDATE Member SET (firstName, lastName, email, password, isSprinter, isDistance, isThrower, isJumper) VALUES ('$_POST[firstName]','$_POST[lastName]','$_POST[email]','$_POST[password]','$_POST[isSprinter]','$_POST[isDistance]','$_POST[isThrower]','$_POST[isDistance]') WHERE memberID = '$_SESSION['memberID']'";
 
             // Update account and send them to the homepage
             mysqli_query($db, $updateQuery);
