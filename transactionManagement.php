c1f2c2a46a2d5647b436ef075d731b2e12d70653
Michael Fink <mfink4@wisc.edu>
2018-04-30 23:25:15 -0500

    A few usability updates.

--- transactionManagement.php
+++ transactionManagement.php
@@ -7,7 +7,7 @@
         <a href='transactionManagementUpdate.php?newTransaction=1' class='btn btn-warning mr-3'>New Transaction</a>
         
         <form class='form-inline' action='transactionManagement.php' name='transactionSearch' method='post'>
-            <input class='form-control mr-3' type='text' placeholder='Search Value' name='transactionSearchValue'>
+            <input class='form-control mr-3' type='text' placeholder='Search Value' name='transactionSearchValue' required>
             <select type="text" class="form-control mr-3" name='transactionSearchType' id='transactionSearchType'>
                 <option selected value="1">Last Name</option>            
                 <option value="2">Transaction ID</option>
---
+++ transactionManagementUpdate.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php   
+        include 'helper/header.php';
+        include 'helper/connect.php';
+        session_start();
+        
+        // Admin is updating a transaction record.
+        if(isset($_GET[transactionID]){
+            
+        }
+        // Admin is creating a new transaction record.
+        else if (isset($_GET[newTransaction]){
+            
+        }
+        $eventResult = $db->("SELECT * FROM Transaction, Member JOIN Member ON transactionApprovalMemberID = memberID WHERE transactionID = $_GET[transactionID]");
+        mysqli_close($db);
+        $row = mysqli_fetch_array($eventResult, MYSQLI_BOTH);
+    ?>
+
+    <body>
+        <div class="container bg-faded">
+            <form class="form-signin" action='helper/accountHelper.php' name='self-update' method='post'>
+                <center><h1> User Account Panel </h1></center>
+
+                <div class="form-label-group">
+                    <input type="text" id="inputFirstName" class="form-control" name='firstName' value=<?php echo "'$_SESSION[firstName]'";?> required>
+                    <label for="inputFirstName">First Name</label>
+                </div>
+
+                <div class="form-label-group">
+                    <input type="text" id="inputLastName" class="form-control" name="lastName" value=<?php echo "'$_SESSION[lastName]'";?> required>
+                    <label for="inputLastName">Last Name</label>
+                </div>
+
+                <div class="form-label-group">
+                    <input type="email" id="inputEmail" class="form-control" name="email" value=<?php echo "'$_SESSION[email]'";?> required>
+                    <label for="inputEmail">Email Address</label>
+                </div>
+
+                <div class="form-label-group">
+                    <input type="password" id="inputPassword" class="form-control" name="password"  value=<?php echo "'$row[password]'";?> required>
+                    <label for="inputPassword">Password</label>
+                </div>
+
+                <div class='form-check form-check-inline'>
+                    <input class='form-check-input' type='checkbox' id='inputIsSprinter' name='isSprinter' <?php if ($row[isSprinter]) echo "checked";?>>
+                    <label class='form-check-label' for='inputIsSprinter'>Sprinter</label>
+                </div>
+
+                <div class='form-check form-check-inline'>
+                    <input class='form-check-input' type='checkbox' id='inputIsDistance' name='isDistance'<?php if ($row[isDistance]) echo "checked";?>>
+                    <label class='form-check-label' for='inputIsDistance'>Distance</label>
+                </div>
+
+                <div class='form-check form-check-inline'>
+                    <input class='form-check-input' type='checkbox' id='inputIsThrower' name='isThrower'<?php if ($row[isThrower]) echo "checked";?>>
+                    <label class='form-check-label' for='inputIsThrower'>Thrower</label>
+                </div>
+
+                <div class='form-check form-check-inline'>
+                    <input class='form-check-input' type='checkbox' id='inputIsJumper' name='isJumper'<?php if ($row[isJumper]) echo "checked";?>>
+                    <label class='form-check-label' for='inputIsJumper'>Jumper</label>
+                </div>
+
+                <button class="btn btn-lg btn-success btn-block" type="submit" name='self-update'>Update Account</button>
+            </form>
+        </div>
+    </body>
+    <?php include 'helper/footer.php' ?>
+</html>
