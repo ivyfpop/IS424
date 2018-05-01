a3b69ce8eeaec1641d6185433d1841014b1a8fa2
LiamMahoney <liammahoney96@gmail.com>
2018-05-01 01:10:31 -0500

    Button name?

--- eventSignUp.php
+++ eventSignUp.php
@@ -61,7 +61,7 @@ include 'helper/connect.php';
   echo"
     <input type='hidden' name='eventID' value=$_POST[eventID]>
     <input type='hidden' name='registeredID' value=$_POST[registeredID]>
-    <button type='submit' class='btn btn-danger eventSignUp'>Confirm Sign Up</button>
+    <button type='submit' name='submitSignUp' class='btn btn-danger'>Confirm Sign Up</button>
   </form>";
   mysqli_close($db);
  ?>
--- myEvents.php
+++ myEvents.php
@@ -10,13 +10,10 @@ include 'helper/header.php';
   <h1 class='text-center'><strong>My Events</strong></h1>
 
 <?php
-//TODO: test this works
-  if(isset($_POST['submitSignUp'])) {
+  if(isset($_POST[submitSignUp])) {
     $errQuery = "INSERT INTO Registered_Member_Event (registeredID, eventID, transactionID, isComplete, carCapacity, leaveBy) VALUES ($_POST[registeredID], $_POST[eventID], NULL, 0, $_POST[numberOfSeatsAvailable], NULL)";
     mysqli_query($db, "INSERT INTO Registered_Member_Event (registeredID, eventID, transactionID, isComplete, carCapacity, leaveBy) VALUES ($_POST[registeredID], $_POST[eventID], NULL, 0, $_POST[numberOfSeatsAvailable], NULL)");
   }
-
-
 
   //Querying for registeredID from Registered_Member with memberID
   $regMemQuery = "SELECT registeredID FROM Registered_Member WHERE memberID = '$_SESSION[memberID]' ORDER BY registeredSeason DESC";
