bb84091dd8545d0f7e0d78b0ee5b2f60aa6b80f2
LiamMahoney <liammahoney96@gmail.com>
2018-05-01 01:40:33 -0500

    never

--- eventSignUp.php
+++ eventSignUp.php
@@ -6,15 +6,8 @@ include 'helper/connect.php';
 /*
   registeredID and eventID in $_POST.
 
-  TODO: Display all information about the event the user is signing up for
-  TODO: If member has a date in driverAuthorizationDate, ask for seats left in car
-  TODO: Create instance in Registered_Member_Event with proper registeredID, eventID
-          -Figure out how transactionID will be populated - some events don't have
-           transactions
-          -Should prices be set in Event table so we can tell the user how expensive
-           the event is?
-  TODO: Redirect to myEvents page when submitted (catch $_POST on myEvents.php page -
-        this is where the update will actually happen)
+  TODO: If member has a date in driverAuthorizationDate, ask for timeToLeaveBy
+          -What to do for user that is not an eligible driver?
 
 */
 ?>
