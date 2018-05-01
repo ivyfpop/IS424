11638c149b86622cacd7d61a7060ae7d0218846e
Michael Fink <mfink4@wisc.edu>
2018-05-01 13:39:32 -0500

    Some Stuff

--- eventSignUp.php
+++ eventSignUp.php
@@ -10,60 +10,57 @@
 */
 include 'helper/header.php';
 session_start();
-include 'helper/connect.php';
 ?>
 
 <div class="container bg-faded p-4 my-4">
-  <h1 class="text-center">Event Details</h1>
-<?php
-  //Querying all details of event with the passed in eventID.
-  $eventDetails = mysqli_query($db, "SELECT * FROM Event WHERE eventID = $_POST[eventID]");
-  //Displaying event details.
-  if ($eventDetails->num_rows == 1) {
-    $row = $eventDetails->fetch_array(MYSQLI_ASSOC);
-    echo"<h3><strong>Event ID:</strong> $row[eventID]";
-    echo"<h3><strong>Event Name:</strong> $row[eventName]";
-    echo"<h3><strong>Date of Event:</strong> $row[eventDate]";
-    echo"<h3><strong>Event Location:</strong> $row[eventAddress] $row[eventCity], $row[eventState] $row[eventZip]";
-    echo"<h3><strong>Description:</strong> $row[eventBio]";
-  }
+    <h1 class="text-center">Event Registration:</h1>
+        <?php
+            //Querying all details of event with the passed in eventID.
+            include 'helper/connect.php';
+            $eventDetails = $db->query("SELECT * FROM Event WHERE eventID = $_POST[eventID]");
+            mysqli_close($db);
+            
+            // Display the event, if there is an event
+            if ($eventRow = $eventDetails->fetch_array(MYSQLI_ASSOC)) {
+                // Display the details of the event they can register for:
+                echo"<h3>
+                        <strong>Event ID:</strong> $eventRow[eventID]
+                        <strong>Event Name:</strong> $eventRow[eventName]
+                        <strong>Event Date:</strong> $eventRow[eventDate]
+                        <strong>Event Location:</strong> $eventRow[eventAddress] $eventRow[eventCity], $eventRow[eventState] $eventRow[eventZip]
+                        <strong>Event Description:</strong> $eventRow[eventBio]
+                        <form action='myEvents.php' method='post'>
+                            <div class='form-group'>
+                                <label for='leaveBy'><strong>Leave By: </strong></label>
+                                <input type='time' name='leaveBy' id='leaveBy' required autofocus>
+                            </div>";
+                        
+                // Only authorized drivers can be asked how many seats they have
+                if ($_SESSION[driverAuthorizationDate] != NULL) {
+                    echo"<div class='form-group'>
+                            <label for='numberOfSeatsAvailable'>(If Driving)Extra Seats:</label>
+                            <select class='form-control' name='numberOfSeatsAvailable' id='numberOfSeatsAvailable'>
+                                <option value='0' selected>0</option>
+                                <option value='1'>1</option>
+                                <option value='2'>2</option>
+                                <option value='3'>3</option>
+                                <option value='4'>4</option>
+                                <option value='5'>5</option>
+                                <option value='6'>6</option>
+                                <option value='7'>7</option>
+                            </select>
+                        </div>";
+                }
+                else{
+                    echo"<strong> Become an authorized driver!</strong>";
+                }
 
-  mysqli_free_result($eventDetails);
-  //Querying for driverAuthorizationDate from Member table
-  $driverStatus = mysqli_query($db, "SELECT driverAuthorizationDate FROM Member WHERE memberID = $_SESSION[memberID]");
-  $driverStatusRow = $driverStatus->fetch_array(MYSQLI_ASSOC);
-  //Asking user for leaveBy time - see top for description.
-  echo"
-  <form action='myEvents.php' method='post'>
-  <div class='form-group'>
-    <label for='leaveBy'><strong>Leave By: </strong></label>
-    <input type='time' name='leaveBy' id='leaveBy' required autofocus>
-  </div>";
-  //Asking user for numberOfSeatsAvailable - see top for description.
-  if ($driverStatusRow[driverAuthorizationDate] != NULL) {
-    echo"
-    <div class='form-group'>
-      <label for='numberOfSeatsAvailable'>Seats available in car</label>
-      <select class='from-control' name='numberOfSeatsAvailable' id='numberOfSeatsAvailable'>
-        <option value='0' selected>0</option>
-        <option value='1'>1</option>
-        <option value='2'>2</option>
-        <option value='3'>3</option>
-        <option value='4'>4</option>
-        <option value='5'>5</option>
-        <option value='6'>6</option>
-        <option value='7'>7</option>
-      </select>
-    </div>";
-  }
-
-  mysqli_free_result($driverStatus);
-  mysqli_close($db);
-  //Including eventID and registeredID allows for easier database updating
-  echo"
-    <input type='hidden' name='eventID' value=$_POST[eventID]>
-    <input type='hidden' name='registeredID' value=$_POST[registeredID]>
-    <input type='submit' class='btn btn-danger' name='submitSignUp' value='Confirm Sign Up'>
-  </form>";
- ?>
+              //Including eventID and registeredID allows for easier database updating
+              echo"
+                <input type='hidden' name='eventID' value=$_POST[eventID]>
+                <input type='hidden' name='registeredID' value=$_POST[registeredID]>
+                <input type='submit' class='btn btn-danger' name='submitSignUp' value='Confirm Sign Up'>
+                <button class='form-control btn btn-success' type='submit' name='transactionSearch'>Search Transactions!</button>
+              </form>";
+         ?>
 </div>
