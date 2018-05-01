c87613542485037c4a757b743c22f3cd196e9269
Michael Fink <mfink4@wisc.edu>
2018-04-30 20:15:34 -0500

    Maybe?

--- transactionManagement.php
+++ transactionManagement.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'helper/header.php'?>
+    <?php include 'helper/header.php'?>
     
     <div class='navbar navbar-dark bg-primary d-flex justify-content-center'>
         <form class='form-inline' action='transactionManagement.php' name='transactionSearch' method='post'>
@@ -17,9 +17,10 @@
     </div>
 
         <?php
-            // Start the session.
+            // Start the session and get ready for database interactions.
             session_start();
-            
+            include 'helper/connect.php';
+
             // Default Query if a post was not entered.
             $transactionQuery = "SELECT * FROM Transaction JOIN Member ON Transaction.memberID = Member.memberID ORDER BY transactionPaymentDate ASC, transactionApprovalDate ASC LIMIT 25";
 
@@ -54,90 +55,71 @@
             }
             // If a transaction approval was made, use the same query.
             else if (isset($_POST['transactionApproval'])){
-                include 'helper/connect.php';
-                $memberID = $_SESSION['memberID'];
-                $transactionID = $_POST['transactionID'];
-                $approveQuery = "UPDATE Transaction SET transactionPaymentDate = NOW(), transactionApprovalDate = NOW(), transactionApprovalMemberID = '$memberID' WHERE transactionID = '$transactionID'";
-                $db->query($approveQuery);
-                mysqli_close();
-                echo"<p>$approveQuery</p>";
+                $db->query("UPDATE Transaction SET transactionPaymentDate = NOW(), transactionApprovalDate = NOW(), transactionApprovalMemberID = '$_SESSION[memberID]' WHERE transactionID = '$_POST[transactionID]'");
                 $transactionQuery = $_POST['transactionQuery'];
             }
                 
                 
                 
             // Connect to the database, run query, and close connection.
-            include 'helper/connect.php';
             $transactions = $db->query($transactionQuery);
             mysqli_close($db);
 
             // If there is only one transaction returned, go right to the modify transaction page.
-            //if($transactions && $transactions->num_rows){
-                // Redirect to the transactionModification.php?transactionID=$POST['transactionValue'];
-            //}
+            if($transactions && $transactions->num_rows == 1 && isset($_POST['transactionSearch'])){
+                header('Location: http://track.finkmp.com/transactionManagementUpdate?transactionID=$_POST[transactionID]');
+            }
             // Verify there are transactions
             if($transactions && $transactions->num_rows){
-                    // Open the containte
-                    echo"<div class='container bg-faded p-4 my-4'>";
+                // Open the containte
+                echo"<div class='container bg-faded p-4 my-4'>";
                 // Loop through all of their transactions
                 while ($row = mysqli_fetch_array($transactions, MYSQLI_BOTH)){
+
                     // Date that the transaction was created.
                     $transactionInitDate = date("m/d/y g:i A", strtotime($row[transactionInitDate]));
                     
                     // The transaction has not been paid yet
                     if($row[transactionPaymentDate] == null){
-                        echo"
-                        <div class='card mb-3 border-danger'>
-                            <div class='card-header bg-danger'>
-                                    <button class='btn btn-link text-white float-left' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
-                                        <h3>$row[transactionID] - PAYMENT DUE - $$row[transactionQuantity]</h3>
-                                    </button>
-
-                                    <a href='transactionManagementUpdate.php?transactionID=$row[transactionID]' class='btn btn-info float-right'><h3>Modify</h3></a>
-
-                                    <form action='transactionManagement.php' name='transactionApproval' method='post'>
-                                        <input type='hidden' name='transactionQuery' value='$transactionQuery'>
-                                        <input type='hidden' name='transactionID' value='$row[transactionID]'>
-                                        <button class='btn btn-success float-right mr-3' type='submit' name='transactionApproval' value='$row[transactionID]'>
-                                            <h3>Approve</h3>
+                        echo"<div class='card mb-3 border-danger'>
+                                <div class='card-header bg-danger'>
+                                        <button class='btn btn-link text-white float-left' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
+                                            <h3>$row[transactionID] - PAYMENT DUE - $$row[transactionQuantity]</h3>
                                         </button>
-                                    </form>
-                            </div>
-                            
-                            <div id='$row[transactionID]' class='collapse'>
-                              <div class='card-body border-warning'>
-                                <strong>Transaction ID:</strong> $row[transactionID]
-                                </br>
-                                <strong>Request Date:</strong> $transactionInitDate
-                                </br>";
-                                
-                                // If there is an Event associated with the transaction
-                                if ($row[eventID]){
-                                    //Determine the name of the event
-                                    $eventResult = $db->query("SELECT eventName FROM Event WHERE eventID = $row[eventID]");
-                                    $eventRow = mysqli_fetch_array($eventResult, MYSQLI_BOTH);
-                                    echo"<strong>Event Name:</strong> $eventRow[eventName]
-                                         </br>";
-                                }
-                                // Otherwise print out the category for the event
-                                else{
-                                    echo"<strong>Category:</strong> $row[transactionCategory]
-                                         </br>";
-                                }
-                                
-                               echo"<strong>Amount:</strong> $$row[transactionQuantity]
-                                    </br>                                            
-                                    <strong>Description:</strong> $row[transactionDescription]
+
+                                        <a href='transactionManagementUpdate.php?transactionID=$row[transactionID]' class='btn btn-info float-right'><h3>Modify</h3></a>
+
+                                        <form action='transactionManagement.php' name='transactionApproval' method='post'>
+                                            <input type='hidden' name='transactionQuery' value='$transactionQuery'>
+                                            <input type='hidden' name='transactionID' value='$row[transactionID]'>
+                                            <button class='btn btn-success float-right mr-3' type='submit' name='transactionApproval' value='$row[transactionID]'>
+                                                <h3>Approve</h3>
+                                            </button>
+                                        </form>
                                 </div>
-                            </div>
-                        </div>";  
+                            
+                                <div id='$row[transactionID]' class='collapse'>
+                                    <div class='card-body border-warning'>
+                                        <strong>Transaction ID:</strong> $row[transactionID]
+                                        </br>
+                                        <strong>Request Date:</strong> $transactionInitDate
+                                        </br>
+                                        <strong>Event Name (If any):</strong> $eventRow[eventName]
+                                        </br>
+                                        <strong>Amount:</strong> $$row[transactionQuantity]
+                                        </br>                                            
+                                        <strong>Description:</strong> $row[transactionDescription]
+                                    </div>
+                                </div>
+                            </div>";  
                     }
                     // The transaction is pending.
                     else if($row[transactionApprovalDate] == NULL){
-                        
+                        // Date assocaited with a pending transaction
                         $transactionPaymentDate = date("m/d/y g:i A", strtotime($row[transactionPaymentDate]));
+
                         echo"<div class='card mb-3 border-warning'>
-                            <div class='card-header bg-warning'>
+                                <div class='card-header bg-warning'>
                                     <button class='btn btn-link text-white float-left' type='button' data-toggle='collapse' data-target='#$row[transactionID]'>
                                         <h3>$row[transactionID] - PENDING - $$row[transactionQuantity]</h3>
                                     </button>
@@ -151,38 +133,25 @@
                                             <h3>Approve</h3>
                                         </button>
                                     </form>
-                            </div>
-                            
-                            <div id='$row[transactionID]' class='collapse'>
-                              <div class='card-body border-warning'>
-                                <strong>Transaction ID:</strong> $row[transactionID]
-                                </br>
-                                <strong>Request Date:</strong> $transactionInitDate
-                                </br>
-                                 <strong>Payment Date:</strong> $transactionPaymentDate
-                                </br>";
-                                
-                                // If there is an Event associated with the transaction
-                                if ($row[eventID]){
-                                    //Determine the name of the event
-                                    $eventResult = $db->query("SELECT eventName FROM Event WHERE eventID = $row[eventID]");
-                                    $eventRow = mysqli_fetch_array($eventResult, MYSQLI_BOTH);
-                                    echo"<strong>Event Name:</strong> $eventRow[eventName]
-                                         </br>";
-                                }
-                                // Otherwise print out the category for the event
-                                else{
-                                    echo"<strong>Category:</strong> $row[transactionCategory]
-                                         </br>";
-                                }
-                                
-                               echo"<strong>Amount:</strong> $$row[transactionQuantity]
-                                    </br>                                            
-                                    <strong>Description:</strong> $row[transactionDescription]
                                 </div>
-                            </div>
-                        </div>";             
-                       
+                                
+                                <div id='$row[transactionID]' class='collapse'>
+                                    <div class='card-body border-warning'>
+                                        <strong>Transaction ID:</strong> $row[transactionID]
+                                        </br>
+                                        <strong>Request Date:</strong> $transactionInitDate
+                                        </br>
+                                        <strong>Payment Date:</strong> $transactionPaymentDate
+                                        </br>
+                                        <strong>Event Name (If any):</strong> $eventRow[eventName]
+                                        </br>
+                                        <strong>Amount:</strong> $$row[transactionQuantity]
+                                        </br>                                            
+                                        <strong>Description:</strong> $row[transactionDescription]
+                                    </div>
+                                </div>
+                            </div>";             
+                           
                         
                     }
                     // The transaction has been approved.
@@ -206,34 +175,17 @@
                                     </br>
                                     <strong>Request Date:</strong> $transactionInitDate
                                     </br>
-                                     <strong>Payment Date:</strong> $transactionPaymentDate
+                                    <strong>Payment Date:</strong> $transactionPaymentDate
                                     </br>
-                                     <strong>Approval Date:</strong> $transactionApprovalDate
-                                    </br>";
-                                    
-                                    //Print out the approving officer's name
-                                    $approvalMemberResult = $db->query("SELECT firstName, lastName FROM Member WHERE memberID = $row[transactionApprovalMemberID]");
-                                    $approvalMemberRow = mysqli_fetch_array($approvalMemberResult, MYSQLI_BOTH);
-                                    echo"<strong>Approving Officer:</strong> $approvalMemberRow[firstName] $approvalMemberRow[lastName]
-                                         </br>";
-                                    
-                                    // If there is an Event associated with the transaction
-                                    if ($row[eventID]){
-                                        //Determine the name of the event
-                                        $eventResult = $db->query("SELECT eventName FROM Event WHERE eventID = $row[eventID]");
-                                        $eventRow = mysqli_fetch_array($eventResult, MYSQLI_BOTH);
-                                        echo"<strong>Event Name:</strong> $eventRow[eventName]
-                                             </br>";
-                                    }
-                                    // Otherwise print out the category for the event
-                                    else{
-                                        echo"<strong>Category:</strong> $row[transactionCategory]
-                                             </br>";
-                                    }
-                                    
-                                   echo"<strong>Amount:</strong> $$row[transactionQuantity]
-                                        </br>                                            
-                                        <strong>Description:</strong> $row[transactionDescription]
+                                    <strong>Approval Date:</strong> $transactionApprovalDate
+                                    </br>
+                                    <strong>Approving Officer:</strong> $row[firstName] $approvalMemberRow[lastName]
+                                    </br>
+                                    <strong>Event Name (If any):</strong> $row[eventName]
+                                    </br>
+                                    <strong>Amount:</strong> $$row[transactionQuantity]
+                                    </br>                                            
+                                    <strong>Description:</strong> $row[transactionDescription]
                                     </div>
                                 </div>
                             </div>";             
@@ -247,7 +199,6 @@
             else if($transactionQuery){
                 echo"<div class='alert alert-warning text-center mx-auto text-center w-50' role='alert'><strong>Your search returned no results, please try again!</strong></div>";                    
             }
-        ?>
-    
-  <?php include 'helper/footer.php' ?>
+            include 'helper/footer.php'
+    ?>
 </html>
