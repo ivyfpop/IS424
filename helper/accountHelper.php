218371b23960cdbd72dc2c69fc1f09caa22a047b
Michael Fink <mfink4@wisc.edu>
2018-05-01 14:36:18 -0500

    wef

--- helper/accountHelper.php
+++ helper/accountHelper.php
@@ -5,7 +5,7 @@
         if(isset($_POST['login'])){
             // Connect to the database, run query, close connection
             include 'connect.php';
-            $result = mysqli_query($db,"SELECT memberID,adminStatus,firstName,lastName,email FROM Member WHERE email = '$_POST[email]' AND password = '$_POST[password]'");
+            $result = mysqli_query($db,"SELECT memberID,adminStatus,firstName,lastName,email,driverAuthorizationDate FROM Member WHERE email = '$_POST[email]' AND password = '$_POST[password]'");
             mysqli_close($db);
 
             //Verify that the there is a user and store the session data if so.
@@ -17,13 +17,14 @@
                 $_SESSION['firstName'] = $row['firstName'];
                 $_SESSION['lastName'] = $row['lastName'];
                 $_SESSION['email'] = $row['email'];
-                
+                $_SESSION['email'] = $row['email'];
+                $_SESSION['driverAuthorizationDate'] = $row['driverAuthorizationDate'];
                 // Determine the member is registered for the current season
                 include 'connect.php';
                 $registeredResult = mysqli_query($db,"SELECT * FROM Registered_Member WHERE memberID = '$row[memberID]' ORDER BY registeredSeason DESC LIMIT 1");
                 mysqli_close($db);
                 // Current season is hard coded right now. Will need to create season table in the database.
-                if($registeredResult->num_rows == 1 && strcmp($row['registeredSeason'],"2017-2018") == 0){
+                if($registeredResult->num_rows == 1 && !strcmp($row['registeredSeason'],"2017-2018")){
                     $_SESSION['registeredSeason'] = $row['registeredSeason'];
                     $_SESSION['fallDuesDate'] = $row['registeredSeason'];
                     $_SESSION['springDuesDate'] = $row['springDuesDate'];
