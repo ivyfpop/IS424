7152aa06da8e787e40f482ab89915689ac424227
LiamMahoney <liammahoney96@gmail.com>
2018-05-02 00:34:50 -0500

    Driver List

--- eventManagementDetails.php
+++ eventManagementDetails.php
@@ -22,12 +22,42 @@
             echo "Number of members attending: " . $_POST['sumMembers'] . "</br>";
             echo "Number of Drivers: " . $combinedRow[0] . "</br>";
 
+            mysqli_free_result($combinedRow);
+
             //List of drivers
-            
-
-
-            //List of all members signed up for event
             $eventID = $_POST['eventID'];
+
+            $driverListResult = mysqli_query($db, "SELECT Member.memberID, Member.firstName, Member.lastName, Member.email, Member.driverAuthorizationDate FROM Registered_Member_Event JOIN Registered_Member ON Registered_Member_Event.registeredID = Registered_Member.registeredID JOIN Member ON Registered_Member.memberID = Member.memberID WHERE Registered_Member_Event.eventID = $eventID AND NOT Registered_Member_Event.carCapacity = 0");
+
+            //Opening table
+            echo"
+            <h3 class='text-center'>All Drivers Signed Up for Event:</h3>
+            <table class='table'>
+                <thead>
+                    <tr>
+                        <th>Member ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Driver Auth Submission</th>
+                    </tr>
+                </thead>
+                <tbody>";
+
+            while($driverRow = mysqli_fetch_array($driverListResult, MYSQLI_BOTH)) {
+                //Populating rows of table
+                echo"
+                    <tr>
+                        <td>$driverRow[memberID]</td>
+                        <td>$driverRow[firstName] $driverRow[lastName]</td>
+                        <td>$driverRow[email]</td>
+                        <td>$driverRow[driverAuthorizationDate]</td>
+                    </tr>";
+            }
+            echo"</tbody></table>"; //closing table body and table
+
+            mysqli_free_result($driverListResult);
+
+            //List of all members signed up for event & member details
             $eventMemberResult = mysqli_query($db, "SELECT Member.memberID, Member.firstName, Member.lastName FROM Registered_Member_Event JOIN Registered_Member ON Registered_Member_Event.registeredID=Registered_Member.registeredID JOIN Member ON Registered_Member.memberID=Member.memberID WHERE Registered_Member_Event.eventID=$eventID ORDER BY Member.firstName ASC");
             echo"
             <h3 class='text-center'>All Members Signed Up for Event:</h3>
@@ -47,6 +77,8 @@
                         <td>$innerRow[firstName] $innerRow[lastName]</td>
                     </tr>";
             }
+            mysqli_free_result($eventMemberResult);
+            mysqli_close($db);
             echo"</tbody></table>"; //closing table body and table
         }
 
