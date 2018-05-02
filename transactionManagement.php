aa2f702ef206e1fbde705d01c1e8b3ae181471cf
Michael Fink <mfink4@wisc.edu>
2018-05-01 19:48:54 -0500

    d

--- transactionManagement.php
+++ transactionManagement.php
@@ -9,8 +9,8 @@
 
             <button class='form-control btn btn-warning' type='submit'>New Transaction</button>
 
-            <input type="number" id="inputMemberID" class="form-control" name='memberID' placeholder='Member ID' required>
-            <input type="number" id="inputQuantity" class="form-control" name='transactionQuantity' placeholder='quantity' required>
+            <input type="number" id="inputMemberID" class="form-control ml-3" name='memberID' placeholder='Member ID' required>
+            <input type="number" id="inputQuantity" class="form-control ml-3" name='transactionQuantity' placeholder='quantity' required>
             
         </form>
 
