020e631b703010de234379ca4a71ea8366f6e155
jfortman4 <jfortman22@gmail.com>
2018-05-02 09:43:13 -0500

    welcome message

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php include 'helper/header.php' ?>    
+    <?php include 'helper/header.php' ?>
     <?php include 'helper/footer.php' ?>
+
+    <div class='container bg-faded p-4 my-4'>
+      <h1 class='text-center'><strong>Welcome</strong></h1>
+      <h2 class='text-center'><strong>to the 2018 Track and Field Season</strong></h2>
+    </div>
 </html>
